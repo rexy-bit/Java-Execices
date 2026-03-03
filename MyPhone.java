fdd680a6264dda868bcaddf4199f43153c951232
Yanis Rezgui <yanisrezgui28@gmail.com>
2026-03-04 00:12:07 +0100

    Add files via upload

---
+++ MyPhone.java
@@ -0,0 +1,12 @@
+package tp1;
+
+public class MyPhone {
+    int storage;
+    int price;
+    String brand;
+    public  void display() {
+       System.out.print("\nBrand : " + brand);
+       System.out.print("\nPrice : " + price);
+       System.out.print("\nStorage : " +  storage);
+    		   }
+    }
