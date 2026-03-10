58168fc8baf115ded56e55e3f42be0683ad6e628
Yanis Rezgui <yanisrezgui28@gmail.com>
2026-03-11 00:08:35 +0100

    Add files via upload

---
+++ Exo4.java
@@ -0,0 +1,14 @@
+package TP2;
+
+public class Exo4 {
+	
+	public static void main(String[] args) {
+		
+		for(int i = 1 ; i<=50;i++) {
+			if(i % 2 == 0) {
+				System.out.print("i = " + i);
+			}
+		}
+	}
+
+}
