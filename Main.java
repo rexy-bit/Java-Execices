0bd4dd9d5b2724ce9a8df1c243a73a16fa1e8570
Yanis Rezgui <yanisrezgui28@gmail.com>
2026-03-11 00:09:18 +0100

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,23 +1,15 @@
-package tp1;
-
-import java.util.Scanner;
+package TP1;
 
 public class Main {
+	
+	public static void main(String[] args) {
+		
+	
 
-    public static void main(String[] args) {
-
-        MyPhone p = new MyPhone();
-        Scanner S = new Scanner(System.in);
-
-        System.out.print("Enter the brand : ");
-        p.brand = S.nextLine();
-
-        System.out.print("Enter the price : ");
-        p.price = S.nextInt();
-
-        System.out.print("Enter the storage : ");
-        p.storage = S.nextInt();
-
-        p.display();
-    }
+	Rectangle R = new Rectangle();
+	
+	
+	R.perimeter();
+	R.area();
+	}
 }
