661e1be493b7d3a0f843f00940a9b1936daa4551
Yanis Rezgui <yanisrezgui28@gmail.com>
2026-03-15 23:16:34 +0100

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,15 +1,13 @@
-package TP1;
+package Tp1;
 
 public class Main {
-	
-	public static void main(String[] args) {
-		
-	
+    
+    public static void main(String[] args){
 
-	Rectangle R = new Rectangle();
-	
-	
-	R.perimeter();
-	R.area();
-	}
+        Rectangle r = new Rectangle();
+
+        r.area();
+        r.perimeter();
+
+    }
 }
