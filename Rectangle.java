5e72915a5c4b696df6f4a30e0a785a2c810808d7
Yanis Rezgui <yanisrezgui28@gmail.com>
2026-03-15 23:16:42 +0100

    Add files via upload

--- Rectangle.java
+++ Rectangle.java
@@ -1,37 +1,33 @@
-package TP1;
+package Tp1;
 
 import java.util.Scanner;
 
 public class Rectangle {
+    
+    int length;
+    int width;
 
-	int length;
-	int width;
-	
-	Scanner S = new Scanner(System.in);
-	
-	public void area() {
-		
-		System.out.print("Enter the length : \n");
-		length = S.nextInt();
-		
-		System.out.print("Enter the width : ");
-		width = S.nextInt();
-		
-		System.out.println("The are is : " + (length*width));
-		
-	}
-	
-	public void perimeter() {
-		
-		System.out.println("Enter the length : \n");
-		length = S.nextInt();
-		
-		System.out.println("Enter the width : \n");
-		width = S.nextInt();
-		
-		System.out.println("The Perimeter is : " + ((length + width)*2));
-		
-	}
-	
-	
+    Scanner S = new Scanner(System.in);
+
+
+    public void area(){
+
+        System.out.println("Enter the length : ");
+        length = S.nextInt();
+
+        System.out.println("Enter the width : ");
+        width = S.nextInt();
+        System.out.println("The area is : " + (length*width));
+
+    }
+
+    public void perimeter(){
+
+        System.out.println("Enter the length : ");
+        length = S.nextInt();
+
+        System.out.println("Enter the width : ");
+        width = S.nextInt();
+        System.out.println("The perimeter is : " + ((length + width)*2));
+    }
 }
